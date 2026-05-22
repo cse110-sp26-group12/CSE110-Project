35258f0dc2747bcf3a7fa85f8e825d733b03ae7d
j4robles <j4robles@ucsd.edu>
2026-05-22 15:49:04 -0700

    Prettify main docs but preserve research prototypes

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,10 @@
 import js from '@eslint/js';
 
 export default [
+  { 
+    ignores: ['docs/research/**'] 
+  },
+
   js.configs.recommended,
   {
     ignores: ['node_modules/**', 'dist/**', 'coverage/**'],
