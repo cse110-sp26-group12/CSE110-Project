0b86c781b26494a0d8db54f461c706d308a320bd
j4robles <j4robles@ucsd.edu>
2026-05-31 18:00:03 -0700

    corrected dataRepository.js reference

--- data-repo/dataRepository.js
+++ data-repo/dataRepository.js
@@ -4,4 +4,4 @@ export * from './repos/sessionRepository.js'; //import { sessionRepo }
 export * from './repos/userRepository.js'; //import { userRepo }
 export * from './repos/teamRepository.js'; //import { teamRepo }
 export * from './repos/teamMemberRepository.js'; //import { teamMemberRepo }
-export * from './repos/standupsRepository.js'; //import { standupsRepo }
+export * from './repos/standupsRepository.js'; //import { standupRepo }
